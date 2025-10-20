21f714302a9bdbc2913a62a777701e84658cf557
Anthony Rattigan <tonyratt24@gmail.com>
2025-10-20 12:12:43 -0400

    start love melee upgrade

--- Assets/Scripts/Player/Upgrades/MeleeUpgrades.cs
+++ Assets/Scripts/Player/Upgrades/MeleeUpgrades.cs
@@ -5,6 +5,7 @@ public class MeleeUpgrades : Upgrade
     float attackSpeedBuff, critChance, critMult; //joy upgrades
     int buffStackCap; float buffPercent; //anger upgrades
     float dotTickDmg, dotMaxTicks; //sadness upgrades
+    float lsAmt; bool lsDoubleActive; //love upgrades
 
     protected override void Awake()
     {
@@ -29,10 +30,14 @@ public class MeleeUpgrades : Upgrade
                 critMult = 1.5f;
                 break;
             case 2:
+                critMult = 1.5f;
                 critChance = 0.3f;
                 break;
             case 3:
                 critMult = 2f;
+                break;
+            default:
+                Debug.LogError("upgrade level out of desired range");
                 break;
         }
     }
@@ -51,9 +56,13 @@ public class MeleeUpgrades : Upgrade
                 break;
             case 2:
                 buffStackCap = 20;
+                buffPercent = 0.01f;
                 break;
             case 3:
                 buffPercent = 0.02f;
+                break;
+            default:
+                Debug.LogError("upgrade level out of desired range");
                 break;
         }
     }
@@ -72,15 +81,40 @@ public class MeleeUpgrades : Upgrade
                 break;
             case 2:
                 dotTickDmg = 1;
+                dotMaxTicks = 5;
                 break;
             case 3:
                 dotMaxTicks = 10;
+                break;
+            default:
+                Debug.LogError("upgrade level out of desired range");
                 break;
         }
     }
 
     protected override void LoveChange()
-    { }
+    {
+        switch (upgradeLevel)
+        {
+            case 0:
+                lsAmt = 0;
+                lsDoubleActive = false;
+                break;
+            case 1:
+                lsAmt = 0.05f;
+                break;
+            case 2:
+                lsAmt = 0.1f;
+                lsDoubleActive = false;
+                break;
+            case 3:
+                lsDoubleActive = true;
+                break;
+            default:
+                Debug.LogError("upgrade level out of desired range");
+                break;
+        }
+    }
 
     protected override void FearChange()
     { }
