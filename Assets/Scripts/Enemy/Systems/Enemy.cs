d1b984aa1fa6da6c24efb83711a736be7e21dfb1
Anthony Rattigan <tonyratt24@gmail.com>
2025-10-20 15:23:30 -0400

    added enemy abstract class
    stores current and max health, and has functionality to support Sadness Melee Effect

---
+++ Assets/Scripts/Enemy/Systems/Enemy.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] float maxHealth;
+    [SerializeField] float currentHealth;
+
+    float dotTickDmg; //dmg per tick
+    int dotMaxTicks; //how many ticks per hit
+    float dotMaxStacks; //max number of dmg stacks
+    float dotTickTimer; //time till next tick
+    float dotTickMaxTime; //time between ticks
+    bool dotActive; //currently taking dmg over time
+    List<int> dotStacks = new List<int>(); //list to store the dot stacks
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if (dotActive)
+        {
+            if (dotTickTimer < 0)
+            {
+                TakeDOT();
+                ResetTimer();
+            }
+        }
+    }
+    public void applyDOT(float dotTickDMG, int dotMaxTicks) //apply the dot effect if not at max stacks, set active, start timer
+    {
+        this.dotTickDmg = dotTickDMG;
+        this.dotMaxTicks = dotMaxTicks;
+
+        if (dotStacks.Count < dotMaxStacks) dotStacks.Add(dotMaxTicks);
+
+        dotActive = true;
+        ResetTimer();
+    }
+
+    void TakeDOT() //take dot damage, reduce all stacks by 1, remove stacks that are now 0, set inactive if no stacks remaining, else reset timer
+    {
+        for (int i = dotStacks.Count; i > 0; i--) //reverse iteration to prevent out of bounds errors
+        {
+            currentHealth -= dotTickDmg;
+            dotStacks[i]--;
+
+            if (dotStacks[i] == 0) dotStacks.RemoveAt(i);
+        }
+
+        if (dotStacks.Count == 0) dotActive = false;
+        if (currentHealth <= 0) Die();
+    }
+
+    void ResetTimer() => dotTickTimer = dotTickMaxTime;
+
+    void Die() { } //override
+
+}
