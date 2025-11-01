4179f86bae933b12c6f6108d004b937d2378f41d
mounirziad <mo697861@ucf.edu>
2025-11-01 16:14:05 -0400

    Mini Map Mock Up

---
+++ Assets/Scripts/Utility/MiniMapScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
