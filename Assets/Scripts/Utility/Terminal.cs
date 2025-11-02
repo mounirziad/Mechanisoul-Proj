c291382eaaa74f13af608810307f818b53fda4bf
Anthony Rattigan <tonyratt24@gmail.com>
2025-11-02 09:52:23 -0500

    added terminal logic
    terminal detects whether player is in range
    
    set up to implement opening ui when button is pressed while in range

---
+++ Assets/Scripts/Utility/Terminal.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+
+public class Terminal : MonoBehaviour
+{
+    [SerializeField] bool inRange;
+
+    private void Awake() => inRange = false;
+
+    private void Update()
+    {
+        if (inRange)
+        {
+            //show visual ui for terminal "press e to open terminal"
+            Debug.Log("in terminal range");
+
+            //if interact key is pressed, open terminal
+        }
+    }
+    private void OnTriggerEnter(Collider other) => inRange = other.CompareTag("Player") ? true : false;
+    private void OnTriggerExit(Collider other) => inRange = other.CompareTag("Player") ? false : true;
+}
