80fe6e8cc5f272669fc5c6aafba034f0661cc756
kingkaue <kaueking@gmail.com>
2025-11-03 21:19:06 -0500

    fixed ui

--- Assets/Scripts/Utility/TempGameManager.cs
+++ Assets/Scripts/Utility/TempGameManager.cs
@@ -17,6 +17,7 @@ public class TempGameManager : MonoBehaviour
         {
             upgradeUIScript.pauseMenu.style.display = DisplayStyle.None;
         }
+
         pauseActive = false;
     }
 
