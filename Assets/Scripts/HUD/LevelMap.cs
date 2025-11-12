17102424d58094288ccdb19687ba0581a60b5b4e
Anthony Rattigan <tonyratt24@gmail.com>
2025-11-11 21:08:54 -0500

    Start Room Map Implementation

--- Assets/Scripts/HUD/LevelMap.cs
+++ Assets/Scripts/HUD/LevelMap.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class LevelMap : MonoBehaviour
+public class Room
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string Name { get; }
+    public string Description { get; set; }
+    public bool cleared;
+
+    public Room(string name, string description = "")
     {
-        
+        Name = name;
+        Description = description;
+        cleared = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override string ToString() => Name;
+
+    public void ClearRoom() => cleared = true;
+}
+
+public class LevelMap : MonoBehaviour
+{
+    //temporary room names, replace with actual rooms later
+    private static readonly Room[,] Rooms =
+            {
+                { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+                {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+                {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+            };
+
+    string currentRoom;
+    
+    public void EnterRoom(string name) => currentRoom = name;
+
+    void UpdateUI()
     {
-        
+        //change ui to reflect new status
+
+        //rooms cleared
+
+        //current room
     }
 }
